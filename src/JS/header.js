fa9e5325722820b97a7999145ff6b34bb9fe8892
ArturMand <ArturMandryk@gmail.com>
2023-02-22 19:42:25 +0200

    addes header and hero

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,82 +1,14 @@
-export function mobileMenu (){
-    let btnModal = document.querySelector('.menu')
-    let modalWindow = document.querySelector('.mobile-menu__container')
-    let coverHero = document.querySelector('.hero__container')
+(() => {
+  const btnOpenMenuRef = document.querySelector('.btn__menu');
+  const mobileMenuRef = document.querySelector('.menu');
+  const heroContentRef = document.querySelector('.hero__wrapper');
+  btnOpenMenuRef.addEventListener('click', () => {
+    const expanded =
+      btnOpenMenuRef.getAttribute('aria-expanded') === 'true' || false;
+      heroContentRef.classList.toggle('is-visible')
+    btnOpenMenuRef.classList.toggle('opened');
+    btnOpenMenuRef.setAttribute('aria-expanded', !expanded);
 
-    const toggleMenu = () => {
-        modalWindow.classList.toggle('mobile-menu__container--closed');
-        coverHero.classList.toggle('hero--closed')
-    }
-  const closeOnESC = (e) => {
-      if (e.keyCode === 27) {
-        modalWindow.classList.add('mobile-menu__container--closed')
-        coverHero.classList.remove('hero--closed')
-        btnModal.classList.remove('opened')
-      }
-  }
-  const closeOnClick = (e) => {
-    e.stopPropagation();
-      modalWindow.classList.add('mobile-menu__container--closed')
-      coverHero.classList.remove('hero--closed')
-      btnModal.classList.remove('opened')
-
-  }
-
-    modalWindow.addEventListener('click', closeOnClick)
-    document.body.addEventListener('keydown', closeOnESC);
-    btnModal.addEventListener('click', toggleMenu);
-
-      window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    modalWindow.classList.add('mobile-menu__container--closed');
-    coverHero.classList.remove('hero--closed')
-
+    mobileMenuRef.classList.toggle('is-open');
   });
-}
-
-
-export function submitForm (){
-  let btnModal = document.querySelector('.hero__button')
-  let modalWindow = document.querySelector('.hero__backdrop')
-  let coverHero = document.querySelector('.hero__container')
-  let closeBtn = document.querySelector('.modal__button-close')
-  let submitBtn = document.querySelector('.submitButton')
-
-  let subBtnTours = document.querySelector('.tours__button--pay')
-
-  const openSubmitForm = () => {
-    modalWindow.classList.remove('is-hidden');
-    coverHero.classList.add('hero--closed')
-  }
-  const closeSubmitForm=()=>{
-    modalWindow.classList.add('is-hidden')
-    coverHero.classList.remove('hero--closed')
-  }
-  const closeOnESC = (e) => {
-      if (e.keyCode === 27) {
-    modalWindow.classList.add('is-hidden')
-    coverHero.classList.remove('hero--closed')
-      }
-  }
-  // const closeOnClick = (e) => {
-  //   e.stopPropagation();
-  //   if (e.target === `<button>`) {
-  //     console.log(e.currentTarget);
-  //     console.log(e.target);
-  //     modalWindow.classList.add('is-hidden')
-  //     coverHero.classList.remove('hero--closed')
-  //   }
-
-  // }
-
-    // modalWindow.addEventListener('click', closeOnClick)
-    document.body.addEventListener('keydown', closeOnESC);
-    
-
-  btnModal.addEventListener('click', openSubmitForm);
-  closeBtn.addEventListener('click', closeSubmitForm)
-  submitBtn.addEventListener('click', closeSubmitForm)
-
-  subBtnTours.addEventListener('click', openSubmitForm);
-
-}
+})();
