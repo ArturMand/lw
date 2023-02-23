1c004548e09249bfd2304385b0786061b7a7c277
ArturMand <108627874+ArturMand@users.noreply.github.com>
2023-02-23 15:56:14 +0000

    Deploying to gh-pages from @ ArturMand/lw@eb32fae0d90e0a83460dc038dbb6026ad89222ec 🚀

--- index.62a1d1e9.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector(".btn__menu"),t=document.querySelector(".menu"),s=document.querySelector(".hero__wrapper");e.addEventListener("click",(()=>{const r="true"===e.getAttribute("aria-expanded")||!1;s.classList.toggle("is-visible"),e.classList.toggle("opened"),e.setAttribute("aria-expanded",!r),t.classList.toggle("is-open")}))})();
-//# sourceMappingURL=index.62a1d1e9.js.map
